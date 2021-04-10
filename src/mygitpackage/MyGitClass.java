5db1bb0aabb9d0c797ab6e8b1985b41017be4770
unknown <handekcknl@gmail.com>
2021-04-10 18:56:34 +0300

    hande versiyon

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -3,6 +3,7 @@ package mygitpackage;
 public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
+		System.out.println("hande versiyon satir 6");
 		
 	}
 
