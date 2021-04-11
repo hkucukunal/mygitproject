dc6d187c6f1581b9660af7fac13455905c1d8598
hkucukunal <81619425+hkucukunal@users.noreply.github.com>
2021-04-11 12:50:34 +0300

    Update MyGitClass.java

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -4,6 +4,8 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
 		System.out.println("hande versiyon satir 6");
+		System.out.println("hande versiyon satir 7");
+		
 		
 	}
 
